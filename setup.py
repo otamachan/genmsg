608b01ce513f3742201a5e0eb9a21528211c4bd5
Tamaki Nishino <otamachan@gmail.com>
2019-08-20 07:21:04 +0900

    Add dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['genmsg'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    setup_requires=['catkin_pkg'],
 )
 
 setup(**d)
